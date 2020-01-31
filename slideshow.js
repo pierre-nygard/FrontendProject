6561e4edd093deaf11e3f31cd082e2b6d0f1e96f
Pierre Nygård <pierre.nygard@gmail.com>
2020-01-31 22:38:07 +0100

    testar js

--- slideshow.js
+++ slideshow.js
@@ -1,13 +1,47 @@
-const images = document.getElementsByClassName('slider__image');
-const links = document.getElementsByClassName('go-to-next');
-
-var activeImage = 1;
+const slider = document.getElementById("slider");
 
 function goNext() {
-    
+    alert("next");
 }
 
-for(var i = 0; i < links.length; i++)
-{
-    links[i].addEventListener("click", goNext)
+function goBack() {
+    alert("back");
 }
+
+function runApp() {
+    loadSettings();
+    addEventListeners();
+}
+
+function loadSettings() {
+    let settingsurl = "/appsettings.json";
+
+    fetch(settingsurl).
+    then(response => response.json()).
+    then(json =>  {
+        for(var i = 0; i < json.image_count; i++) {
+            let backLink = document.createElement('a');
+            backLink.classList.add('go-back');
+            backLink.innerHTML = 'Go back';
+            backLink.addEventListener("click", goBack);
+
+            let nextLink = document.createElement('a');
+            nextLink.classList.add('go-to-next');
+            nextLink.innerHTML = 'Go to next';
+            nextLink.addEventListener("click", goNext);
+            
+            let image = document.createElement("div");
+            image.classList.add('slider__image');
+            image.dataset.image = i+1;
+            image.appendChild(backLink);
+            image.appendChild(nextLink);
+
+            if(image.dataset.image == json.starting_image) 
+                image.classList.add('slider__image--active');
+
+            slider.appendChild(image);
+        }
+    });
+}
+
+document.onload = runApp();
