6cccb95b775ad81cfbed55fec1e36391260bd5cf
Pierre Nygård <pierre.nygard@gmail.com>
2020-01-31 23:47:03 +0100

    byggt vidare på script

--- slideshow.js
+++ slideshow.js
@@ -1,16 +1,26 @@
 const slider = document.getElementById("slider");
-
-function goNext() {
-    alert("next");
-}
-
-function goBack() {
-    alert("back");
-}
+var activeImageIndex;
 
 function runApp() {
     loadSettings();
-    addEventListeners();
+}
+
+function goToImage(direction) {
+
+    let images = document.getElementsByClassName('slider__image');
+
+    images[activeImageIndex].classList.toggle('slider__image--active');
+
+    activeImageIndex += direction;
+
+    activeImageIndex = activeImageIndex < 0 ? images.length - 1 : activeImageIndex;
+    activeImageIndex = activeImageIndex > images.length - 1 ? 0 : activeImageIndex;
+    
+    //alert(activeImageIndex);
+
+
+
+    images[activeImageIndex].classList.toggle('slider__image--active');
 }
 
 function loadSettings() {
@@ -20,24 +30,33 @@ function loadSettings() {
     then(response => response.json()).
     then(json =>  {
         for(var i = 0; i < json.image_count; i++) {
+            let j = i;
             let backLink = document.createElement('a');
             backLink.classList.add('go-back');
             backLink.innerHTML = 'Go back';
-            backLink.addEventListener("click", goBack);
-
+            backLink.addEventListener("click", () => {
+                goToImage(-1)
+            });
+                
             let nextLink = document.createElement('a');
             nextLink.classList.add('go-to-next');
             nextLink.innerHTML = 'Go to next';
-            nextLink.addEventListener("click", goNext);
-            
+            nextLink.addEventListener("click", () => {
+                goToImage(1)
+            });
+                
             let image = document.createElement("div");
             image.classList.add('slider__image');
-            image.dataset.image = i+1;
+            image.dataset.image = i;
+            image.style.backgroundImage = `url(${json[i].url})`;
             image.appendChild(backLink);
             image.appendChild(nextLink);
 
             if(image.dataset.image == json.starting_image) 
+            {
                 image.classList.add('slider__image--active');
+                activeImageIndex = json.starting_image;
+            }
 
             slider.appendChild(image);
         }
