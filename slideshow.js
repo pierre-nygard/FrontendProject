44716e436966ce6afafb12178a1a88e949f7bc9e
erifredrika <erifredrikargmail.com>
2020-02-05 16:47:21 +0100

    med meny, men måste fixas

--- slideshow.js
+++ slideshow.js
@@ -1,13 +1,104 @@
-const images = document.getElementsByClassName('slider__image');
-const links = document.getElementsByClassName('go-to-next');
+const slider = document.getElementById("slider");
+const buttons = document.getElementById('buttons__list');
+var activeImageIndex;
+var walkActive = false;
+var isZoomed    = false;
+var zoomLocked  = false;
 
-var activeImage = 1;
+function runApp() {
+    loadSettings();
+}
 
-function goNext() {
+function goToImage(idx) {
+    if(isZoomed) return false;
+    walkActive = true;
+
+    toggleSlide(activeImageIndex);
+    toggleSlide(idx);
+    
+    setTimeout(() => {walkActive = false}, 200);
+    activeImageIndex = idx;
+}
+
+function toggleSlide(location) {
+    let sliders = document.getElementsByClassName('slider__slide');
+    let circles = document.getElementsByClassName('circle');
+    sliders[location].classList.toggle('slider__slide--active');
+    circles[location].classList.toggle('circle--active');
+
     
 }
 
-for(var i = 0; i < links.length; i++)
-{
-    links[i].addEventListener("click", goNext)
+function loadSettings() {
+    let settingsurl = "/appsettings.json";
+
+    fetch(settingsurl).
+    then(response => response.json()).
+    then(json =>  {
+        activeImageIndex = json.starting_image;
+        
+        for(let i = 0; i < json.image_count; i++) {
+            let slider__slide = document.createElement('div');
+            let listItem = document.createElement('li');
+            let span = document.createElement('span');
+
+            let menu = document.createElement('div');
+            
+
+            
+            span.classList.add('circle');
+            listItem.addEventListener('click', () => goToImage(i));
+
+            slider__slide.classList.add('slider__slide');
+            slider__slide.dataset.slide = i;
+
+            if(i == json.starting_image){
+                slider__slide.classList.add('slider__slide--active');
+                span.classList.add('circle--active');
+            }
+
+            menu.innerHTML = `
+            <div class="menu">
+            <h1>${json[i].image_title}</h1> ` + 
+            
+            
+            `
+            <p>${json[i].image_description}</p> 
+            </div>
+            `
+
+            slider__slide.innerHTML = `<div class="slider__front"></div>
+            <div class="slider__image" data-image="${i}" style="background-image: url(${json[i].image_url})"></div> `;
+
+            buttons.appendChild(listItem);
+            listItem.appendChild(span);
+            $('#slider').append(slider__slide);
+
+            
+            $('#slider').append(menu);
+            
+
+        }
+    });
+}
+
+document.onload = runApp();
+
+// Zoom event
+document.onclick = (e) => {
+    //console.log("entered");
+    if(walkActive) return false;
+        toggleZoom(e.clientX, e.clientY);
+};
+
+function toggleZoom(x, y) {
+    let obj = $('.slider__image[data-image='+activeImageIndex+']');
+
+    if(!isZoomed) {
+        let origin = x+"px "+y+"px";
+        obj.css("transform-origin", origin);
+    }
+
+    obj.toggleClass('slider__image--zoom');
+    isZoomed = !isZoomed;
 }
