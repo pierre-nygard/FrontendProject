fec5c49f911d1900fcbe43f79376802845f29971
erifredrika <erifredrika@gmail.com>
2020-02-11 15:44:18 +0100

    mergat versioner

--- slideshow.js
+++ slideshow.js
@@ -1,104 +1,103 @@
-const slider = document.getElementById("slider");
-const buttons = document.getElementById('buttons__list');
+const slider = $("#slider");
+const buttons = document.getElementById("buttons__list");
 var activeImageIndex;
 var walkActive = false;
-var isZoomed    = false;
-var zoomLocked  = false;
+var isZoomed = false;
+var zoomLocked = false;
 
 function runApp() {
-    loadSettings();
+  loadSettings();
 }
 
 function goToImage(idx) {
-    if(isZoomed) return false;
-    walkActive = true;
+  if (isZoomed) return false;
+  walkActive = true;
 
-    toggleSlide(activeImageIndex);
-    toggleSlide(idx);
-    
-    setTimeout(() => {walkActive = false}, 200);
-    activeImageIndex = idx;
+  toggleSlide(idx);
+  //toggleSlide(idx);
+
+  setTimeout(() => {
+    walkActive = false;
+  }, 200);
+  activeImageIndex = idx;
 }
 
 function toggleSlide(location) {
-    let sliders = document.getElementsByClassName('slider__slide');
-    let circles = document.getElementsByClassName('circle');
-    sliders[location].classList.toggle('slider__slide--active');
-    circles[location].classList.toggle('circle--active');
-
-    
+  let sliders = document.getElementsByClassName("slider__slide");
+  let circles = document.getElementsByClassName("circle");
+  //sliders[location].classList.toggle('slider__slide--active');
+  circles[location].classList.toggle("circle--active");
+  circles[activeImageIndex].classList.toggle("circle--active");
+  console.log();
+  slider.css("left", "-" + location * 100 + "%");
 }
 
 function loadSettings() {
-    let settingsurl = "/appsettings.json";
+  let settingsurl = "/appsettings.json";
 
-    fetch(settingsurl).
-    then(response => response.json()).
-    then(json =>  {
-        activeImageIndex = json.starting_image;
-        
-        for(let i = 0; i < json.image_count; i++) {
-            let slider__slide = document.createElement('div');
-            let listItem = document.createElement('li');
-            let span = document.createElement('span');
+  fetch(settingsurl)
+    .then(response => response.json())
+    .then(json => {
+      activeImageIndex = json.starting_image;
+      slider.css("width", json.image_count * 100 + "%");
 
-            let menu = document.createElement('div');
-            
+      for (let i = 0; i < json.image_count; i++) {
+        let slider__slide = document.createElement("div");
+        let listItem = document.createElement("li");
+        let span = document.createElement("span");
 
-            
-            span.classList.add('circle');
-            listItem.addEventListener('click', () => goToImage(i));
+        let menu = document.createElement("div");
 
-            slider__slide.classList.add('slider__slide');
-            slider__slide.dataset.slide = i;
+        span.classList.add("circle");
+        listItem.addEventListener("click", () => goToImage(i));
 
-            if(i == json.starting_image){
-                slider__slide.classList.add('slider__slide--active');
-                span.classList.add('circle--active');
-            }
+        slider__slide.classList.add("slider__slide");
+        slider__slide.dataset.slide = i;
+        slider__slide.style.transform = "translate(" + i * 100 + "%, 0)";
 
-            menu.innerHTML = `
+        if (i == json.starting_image) {
+          slider__slide.classList.add("slider__slide--active");
+          span.classList.add("circle--active");
+        }
+
+        menu.innerHTML =
+          `
             <div class="menu">
-            <h1>${json[i].image_title}</h1> ` + 
-            
-            
-            `
+            <h1>${json[i].image_title}</h1> ` +
+          `
             <p>${json[i].image_description}</p> 
             </div>
-            `
+            `;
 
-            slider__slide.innerHTML = `<div class="slider__front"></div>
+        slider__slide.innerHTML = `<div class="slider__front"></div>
             <div class="slider__image" data-image="${i}" style="background-image: url(${json[i].image_url})"></div> `;
 
-            buttons.appendChild(listItem);
-            listItem.appendChild(span);
-            $('#slider').append(slider__slide);
+        buttons.appendChild(listItem);
+        listItem.appendChild(span);
+        $("#slider").append(slider__slide);
 
-            
-            $('#slider').append(menu);
-            
-
-        }
+        $("#slider").append(menu);
+      }
     });
 }
 
 document.onload = runApp();
 
 // Zoom event
-document.onclick = (e) => {
-    //console.log("entered");
-    if(walkActive) return false;
-        toggleZoom(e.clientX, e.clientY);
+document.onclick = e => {
+  //console.log("entered");
+  if (walkActive) return false;
+  toggleZoom(e.clientX, e.clientY);
 };
 
 function toggleZoom(x, y) {
-    let obj = $('.slider__image[data-image='+activeImageIndex+']');
+  let obj = $(".slider__image[data-image=" + activeImageIndex + "]");
 
-    if(!isZoomed) {
-        let origin = x+"px "+y+"px";
-        obj.css("transform-origin", origin);
-    }
+  if (!isZoomed) {
+    let origin = x + "px " + y + "px";
+    obj.css("transform-origin", origin);
+  }
 
-    obj.toggleClass('slider__image--zoom');
-    isZoomed = !isZoomed;
+  obj.toggleClass("slider__image--zoom");
+  isZoomed = !isZoomed;
 }
