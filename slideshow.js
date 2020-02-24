02b713d6f9d50e535a25d326a8b50d540f836831
Pierre Nygård <pierre.nygard@gmail.com>
2020-02-24 17:41:48 +0100

    content lagt som en shape

--- slideshow.js
+++ slideshow.js
@@ -3,7 +3,6 @@ const body = $("body");
 const win = $("#window");
 const buttons = $("#buttons__list");
 
-
 var activeImageIndex;
 var walkActive = false; // 1000ms time frame when image is sliding. State doesn't allow zooming
 var isZoomed = false; // Active when image is zoomed. Doesn't allow new image slide until zoomed out.
@@ -12,25 +11,22 @@ function runApp() {
     loadSettings();
 }
 
-function goToImage(idx, bcgColors) {
-    if (isZoomed) return false;
+function goToImage(idx) {
+    if (isZoomed) toggleZoom(0,0);
     walkActive = true;
 
+    // Give body new background
+    $('body').removeClass('body-'+activeImageIndex);
+    $('body').addClass('body-'+idx);
 
     let circles = document.getElementsByClassName("circle");
     circles[idx].classList.toggle("circle--active");
     circles[activeImageIndex].classList.toggle("circle--active");
     slider.css("left", "-" + idx * 100 + "%");
-    
-    body.css("background", "radial-gradient(#" + bcgColors[0] + ", #" + bcgColors[1] + ")" );
-
 
     setTimeout(() => { walkActive = false}, 1000);
 
     activeImageIndex = idx;
-}
-
-function toggleSlide(location) {
 }
 
 function loadSettings() {
@@ -41,22 +37,20 @@ function loadSettings() {
     .then(json => {
         activeImageIndex = json.starting_image;
         slider.css("width", json.image_count * 100 + "%");
-        $(".credentials").css("color", json.color);
-
-        //for the colors to start with the first images colors
-        let startColors = json[activeImageIndex].colors       
-        body.css("background", "radial-gradient(#" + startColors[0] + ", #" + startColors[1] + ")" );
-
-        
 
         for (let i = 0; i < json.image_count; i++) {
+
             let slider__slide = document.createElement("div");
             let listItem = document.createElement("li");
             let span = document.createElement("span");
-            let imageColors = json[i].colors;        
+
+            let image = json.images[i];
+            
+            // Create background classes with each corresponding gradient
+            $("<style type='text/css'> .body-"+i+"{ background: #" + image.color + "</style>").appendTo("head");
 
             span.classList.add("circle");
-            listItem.addEventListener("click", () => goToImage(i, imageColors));
+            listItem.addEventListener("click", () => goToImage(i));
 
             slider__slide.classList.add("slider__slide");
             slider__slide.dataset.slide = i;
@@ -70,11 +64,13 @@ function loadSettings() {
             slider__slide.innerHTML = `
             <div class="slider__front">
                 <div class="content">
-                    <h1>${json[i].image_title}</h1> 
-                    <p>${json[i].image_description}</p> 
+                    <div>
+                        <h1>${image.image_title}</h1> 
+                        <p>${image.image_description}</p> 
+                    </div>
                 </div>
             </div>
-            <div class="slider__image" data-image="${i}" style="background-image: url(${json[i].image_url})"></div> 
+            <div class="slider__image" data-image="${i}" style="background-image: url(${image.image_url})"></div> 
             `;
 
             buttons.append(listItem);
@@ -82,6 +78,7 @@ function loadSettings() {
 
             $("#slider").append(slider__slide);
         }
+        $("body").toggleClass('body-'+activeImageIndex);
     });
 }
 
