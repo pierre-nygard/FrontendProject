4b833e9c37ac98b7c9b8eee980e0a1911a47e7ff
erifredrika <erifredrikargmail.com>
2020-02-25 12:30:02 +0100

    Delat upp lite

--- slideshow.js
+++ slideshow.js
@@ -3,6 +3,8 @@ const body = $("body");
 const win = $("#window");
 const buttons = $("#buttons__list");
 
+
+
 var activeImageIndex;
 var walkActive = false; // 1000ms time frame when image is sliding. State doesn't allow zooming
 var isZoomed = false; // Active when image is zoomed. Doesn't allow new image slide until zoomed out.
@@ -30,58 +32,61 @@ function goToImage(idx) {
 }
 
 function loadSettings() {
-    let settingsurl = "/appsettings.json";
+    
+let settingsurl = "/appsettings.json";
 
     fetch(settingsurl)
     .then(response => response.json())
     .then(json => {
+
         activeImageIndex = json.starting_image;
-        slider.css("width", json.image_count * 100 + "%");
+        slider.css("width", json.image_count * 100 + "%"); 
 
         for (let i = 0; i < json.image_count; i++) {
 
-            let image = json.images[i];
+        let image = json.images[i];           
+        CreateAppend(i, json.starting_image, image);
 
-            let slider__slide = document.createElement("div");
-                $(slider__slide).addClass("slider__slide");
-
-            let listItem = document.createElement("li");
-                $(listItem).click(() => goToImage(i)); //Adding eventlisteners to all listItems
-
-            let span = document.createElement("span");
-                $(span).addClass("circle");
-                    
-                   
-            slider__slide.dataset.slide = i;
-            slider__slide.style.transform = "translate(" + i * 100 + "%, 0)";
-
-            if (i == json.starting_image) {
-                $(slider__slide).addClass("slider__slide--active");
-                $(span).addClass("circle--active");
-              
-            }
-
-
-            ChangeColorBg(i, image.color);
-            FillSlider_Slide(slider__slide, image.image_title, image.image_description, i, image.image_url);
-            
-           
-            $(buttons).append(listItem);           
-            $(listItem).append(span);        
-            $("#slider").append(slider__slide);
         }
-        $("body").toggleClass('body-'+activeImageIndex);
+
+        $("body").toggleClass('body-'+ activeImageIndex);
+
     });
 }
 
+function CreateAppend(i, starting_image, image){
+
+    let slider__slide = document.createElement("div");
+        $(slider__slide).addClass("slider__slide");
+            $(slider__slide).css("transform", "translate(" + i * 100 + "%, 0)");
+                slider__slide.dataset.slide = i;
+
+    let listItem = document.createElement("li");
+        $(listItem).click(() => goToImage(i)); //Adding eventlisteners to all listItems
+
+    let span = document.createElement("span");
+        $(span).addClass("circle");
+                                  
+        if (i == starting_image) {
+            $(slider__slide).addClass("slider__slide--active");
+            $(span).addClass("circle--active");            
+        }
+
+    ChangeColorBg(i, image.color); 
+    
+    FillSlider_Slide(slider__slide, image.image_title, image.image_description, i, image.image_url);
+
+
+    $(buttons).append(listItem);           
+        $(listItem).append(span);        
+            $("#slider").append(slider__slide);
+
+}
 
 function ChangeColorBg(index, imageColor){
     // Create background classes with each corresponding gradient
-    $("<style type='text/css'> .body-"+index+"{ background: #" + imageColor + "</style>").appendTo("head");
-
-            
+    $("<style type='text/css'> .body-" + index + "{ background: #" + imageColor + "</style>").appendTo("head");          
 }
-
 
 function FillSlider_Slide(slider__slide, image_title, image_description, i, image_url ){
     slider__slide.innerHTML = `
